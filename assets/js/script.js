3ee63568008b18b7ab3025ebba6e15565011a5e7
Kim Van Wyk <kmvanwyk@msn.com>
2022-11-18 00:51:00 -0800

    icon working and today weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 // an array to hold the top 10 cities checked for localStorage
 var theCityArray = new Array();
 //link to element city button in the Dom using id
-var cityBtnEl = $('#cityBtn');
+var cityBtnEl = $('.cityButton');
 //link to element search button in the Dom using id
 var searchBtnEl = $('#searchBtn');
 // link to city button elements section in Dom
@@ -12,6 +12,7 @@ var cityLat;
 var cityLon;
 //key for API
 const apiKey = "7b5031efd51fb04c52651f1ab0b416b0";
+
 
 
 //https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=7b5031efd51fb04c52651f1ab0b416b0
@@ -94,15 +95,9 @@ function loadCityBtns(){
             //create/append a city button 
         var name ="#"+i+"Btn";
         var cityButtonEl = $(name);
-        //set a id to know which element you're grabbing to save in listener function
-        //cityButtonEl.attr('id', i);
         cityButtonEl.show();
-        //cityButtonEl.addClass('cityBtn');
-        //cityButtonEl.attr('value',theCityArray[i]);
         cityButtonEl.html(theCityArray[i]);
-        //.val(theCityArray[i]);
-        //cityButtonEl.attr('type','button');
-        //cityBtnSectionEl.append(cityButtonEl);
+
         }
     //TODO:if buttons made only change text otherwise make buttons if button id does not exist for index yet make it. 
     //TODO:up to 10 buttons make a button with id of saved city array index to look up city from storage and set text when change
@@ -113,25 +108,59 @@ function loadCityBtns(){
 function loadWeather(){
     console.log(cityLat+ " lat in load");
     console.log(cityLon +" lon in load");
+
+    var callStringWeatherToday = ("https://api.openweathermap.org/data/2.5/weather?lat="+cityLat+"&lon="+cityLon+"&units=imperial&&appid="+apiKey);
+   
+    fetch(callStringWeatherToday)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log("this is the weather today data");
+    console.log(data);
+    //load todays weather
+    var iconUrl = "https://openweathermap.org/img/wn/"+data.weather[0].icon +".png";
     
+    $('#cityName').text(data.name+ " "+ data.dt);
+    $('#iconToday').attr("src", iconUrl);
+    $('#todayTemp').text("Temp: " + data.main.temp);
+    $('#todayWind').text("Wind: " + data.wind.speed);
+    $('#todayHumid').text("Humidity: " + data.main.humidity);
+    });
+
     //variable to hold the api call string
-    var callStringWeather = ("https://api.openweathermap.org/data/2.5/forecast?lat="+cityLat+"&lon="+cityLon+"&units=imperial&cnt=6&appid="+apiKey);
- //FIXME:get weather object    
-//TODO:load weather section and 5 days cards for city called
-//FIXME: load today section DATE, ICON, CURRENT TEMP? //HIGH, //LOW, WIND, HUMIDITY
+    var callStringWeather5 = ("https://api.openweathermap.org/data/2.5/forecast?lat="+cityLat+"&lon="+cityLon+"&units=imperial&&appid="+apiKey);
 //FIXME: load next 5 days cards DATE, ICON, HIGH, //LOW, WIND HUMIDITY
 //got name off of text for button less computing than pulling from storage
-    fetch(callStringWeather)
+    fetch(callStringWeather5)
     .then(function (response) {
         return response.json();
     })
     .then(function (data) {
         console.log("this is the weather data");
     console.log(data);
-    //data[0]
-    // for (var i = 0; i < data.length; i++) {
-    //     ;
-    // }
+    //load future weather
+    var cardD;
+    var cardI;
+    var cardT;
+    var cardW;
+    var cardH;
+    var iconUrl
+   // for (var i = 0; i < data.length; i++) {
+
+    for (var i = 0; i < 5; i++) {
+        cardD = "#" + i + "cardTitle";
+        cardI = "#" + i + "cardIcon";
+        cardT = "#" + i + "Temp";
+        cardW = "#" + i + "Wind";
+        cardH = "#" + i + "Humidity";
+        $(cardD).text(data.list[i].dt);
+        iconUrl = "https://openweathermap.org/img/wn/"+data.list[i].weather[0].icon +".png";
+        $(cardI).attr("src", iconUrl);
+        $(cardT).text("Temp: " + data.list[i].wind.speed);
+        $(cardW).text("Wind: " + data.list[i].wind.speed);
+        $(cardH).text("Humidity: " + data.list[i].main.humidity);
+     }
     });
 }
 
@@ -151,7 +180,8 @@ searchBtnEl.on("click", function(){
 //city button CLICK listener
 cityBtnEl.on("click", function(){
     //cityName already validated before put on button so don't need to validate again.
-    var cityName = this.name;
+    var cityName = this.innerHTML;
     //validate city will get the lat and lon and then load the weather in that function
+    console.log(cityName +"buttonPressed");
     validateCity(cityName);
 });
